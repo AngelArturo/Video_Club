745abd12b59f558ee49a60e331a796119d0404f0
AALL <angel_220896@live.com.mx>
2017-10-23 16:49:38 -0500

    Diseño de formulario Login

---
+++ Video Club/Video Club/Forms/FrmLogin.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.X = New System.Windows.Forms.Button()
+        Me.Minimizar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'X
+        '
+        Me.X.BackColor = System.Drawing.Color.Transparent
+        Me.X.BackgroundImage = Global.Video_Club.My.Resources.Resources.x
+        Me.X.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.X.FlatAppearance.BorderSize = 0
+        Me.X.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.X.Location = New System.Drawing.Point(664, -13)
+        Me.X.Name = "X"
+        Me.X.Size = New System.Drawing.Size(40, 50)
+        Me.X.TabIndex = 0
+        Me.X.UseVisualStyleBackColor = False
+        '
+        'Minimizar
+        '
+        Me.Minimizar.BackColor = System.Drawing.Color.Transparent
+        Me.Minimizar.BackgroundImage = Global.Video_Club.My.Resources.Resources.__
+        Me.Minimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Minimizar.FlatAppearance.BorderSize = 0
+        Me.Minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Minimizar.Location = New System.Drawing.Point(635, -17)
+        Me.Minimizar.Name = "Minimizar"
+        Me.Minimizar.Size = New System.Drawing.Size(40, 50)
+        Me.Minimizar.TabIndex = 1
+        Me.Minimizar.UseVisualStyleBackColor = False
+        '
+        'FrmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.BackgroundImage = Global.Video_Club.My.Resources.Resources.Login3
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(700, 525)
+        Me.Controls.Add(Me.Minimizar)
+        Me.Controls.Add(Me.X)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FrmLogin"
+        Me.Text = "FrmLogin"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents X As Button
+    Friend WithEvents Minimizar As Button
+End Class
